30b9854ad5745d42389f28c4619bb222b7efc009
toBeNewbie <1320425613@qq.com>
2016-08-15 10:51:04 +0800

    播放动画过程中，检测版本更新逻辑编码

--- src/com/example/mobilesafe/SplashActivity.java
+++ src/com/example/mobilesafe/SplashActivity.java
@@ -1,69 +1,68 @@
 package com.example.mobilesafe;
 
+import com.example.mobilesafe.spUtils.myConstantValue;
+import com.example.mobilesafe.spUtils.splashUtils;
+
 import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.Window;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationSet;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-
 /**
  * @author Administrator
- *@company Newbie
- *@date 2016-8-15
- *@des 初始化Splash界面。
+ * @company Newbie
+ * @date 2016-8-15
+ * @des 初始化Splash界面。
  */
 public class SplashActivity extends Activity {
 
-	
-	 
 	private TextView splash_version_name;
 	private RelativeLayout rl_splash_root;
 	private String versionName;
 	private AnimationSet mAnimationSet;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        
-        //初始化界面。
-        initView();
-        //显示数据到界面。
-        initDate();
-        
-        initEvent();
-        
-        //设置动画
-        initAnimation();
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		// 初始化界面。
+		initView();
+		// 显示数据到界面。
+		initDate();
+
+		initEvent();
+
+		// 设置动画
+		initAnimation();
+	}
 
 	private void initAnimation() {
-		RotateAnimation ra = new RotateAnimation(0.0f, 360.0f, 
-												Animation.RELATIVE_TO_SELF, 0.5f, 
-												Animation.RELATIVE_TO_SELF, 0.5f);
+		RotateAnimation ra = new RotateAnimation(0.0f, 360.0f,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
 		ra.setDuration(2000);
 		ra.setFillAfter(true);
-		
-		ScaleAnimation sa = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f, 
-												Animation.RELATIVE_TO_SELF, 0.5f, 
-												Animation.RELATIVE_TO_SELF, 0.5f);
+
+		ScaleAnimation sa = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
 		sa.setDuration(2000);
 		sa.setFillAfter(true);
-		
-		AlphaAnimation aa=new AlphaAnimation(0.0f, 1.0f);
+
+		AlphaAnimation aa = new AlphaAnimation(0.0f, 1.0f);
 		aa.setDuration(2000);
 		aa.setFillAfter(true);
-		
-		
+
 		mAnimationSet = new AnimationSet(false);
 		mAnimationSet.addAnimation(sa);
 		mAnimationSet.addAnimation(ra);
@@ -74,20 +73,21 @@ public class SplashActivity extends Activity {
 	/**
 	 * 初始化splash界面
 	 */
-    private void initView() {
-    	// TODO Auto-generated method stub
-         setContentView(R.layout.activity_splash);
-         rl_splash_root = (RelativeLayout) findViewById(R.id.rl_splash_root);
-         splash_version_name = (TextView) findViewById(R.id.tv_splash_version_name);
-    }
+	private void initView() {
+		// TODO Auto-generated method stub
+		setContentView(R.layout.activity_splash);
+		rl_splash_root = (RelativeLayout) findViewById(R.id.rl_splash_root);
+		splash_version_name = (TextView) findViewById(R.id.tv_splash_version_name);
+	}
 
-    /**
-     * 填充splash界面初始化数据
-     */
+	/**
+	 * 填充splash界面初始化数据
+	 */
 	private void initDate() {
 		// TODO Auto-generated method stub
 		try {
-			PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			PackageInfo packageInfo = getPackageManager().getPackageInfo(
+					getPackageName(), 0);
 			versionName = packageInfo.versionName;
 			splash_version_name.setText(versionName);
 		} catch (NameNotFoundException e) {
@@ -97,16 +97,48 @@ public class SplashActivity extends Activity {
 	}
 
 	private void initEvent() {
-		// TODO Auto-generated method stub
-		
+		mAnimationSet.setAnimationListener(new AnimationListener() {
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+				// TODO Auto-generated method stub
+				if (splashUtils.getbBoolean(getApplicationContext(),
+						myConstantValue.AUTO_VERSION_UPDATE, false)) {
+					//版本更新，连接网络，获取数据。
+					checkUpdate();
+				} else {
+
+				}
+
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				// TODO Auto-generated method stub
+				if (splashUtils.getbBoolean(getApplicationContext(),
+						myConstantValue.AUTO_VERSION_UPDATE, false)) {
+					//版本更新。
+					
+				} else {
+					//跳转到主界面
+
+				}
+			}
+		});
 	}
 
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	//检测版本
+	protected void checkUpdate() {
+		// TODO Auto-generated method stub
+		//检测版本，
+		//安装新版本。
+		//跳转界面。
+	}
 
-     
 }
