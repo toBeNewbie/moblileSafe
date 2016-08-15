8a93f11dbee29780edb7c813ba18c9300cdda912
toBeNewbie <1320425613@qq.com>
2016-08-15 14:52:08 +0800

    主界面跳转逻辑编码

---
+++ src/com/example/mobilesafe/activity/HomeActivity.java
@@ -0,0 +1,43 @@
+package com.example.mobilesafe.activity;
+
+import com.example.mobilesafe.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		initView();
+		
+		initAnimation();
+		
+		initDate();
+		
+		initEvent();
+	}
+
+	private void initEvent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initDate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initAnimation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initView() {
+		// TODO Auto-generated method stub
+		setContentView(R.layout.activity_home);
+	}
+}
