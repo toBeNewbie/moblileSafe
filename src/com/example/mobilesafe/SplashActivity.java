ac5175d892dbcd1ef7652a3ce0e038ad0ba731a2
toBeNewbie <1320425613@qq.com>
2016-08-15 09:50:30 +0800

    设置splash相关动画。

--- src/com/example/mobilesafe/SplashActivity.java
+++ src/com/example/mobilesafe/SplashActivity.java
@@ -1,12 +1,16 @@
 package com.example.mobilesafe;
 
 import android.app.Activity;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -24,18 +28,48 @@ public class SplashActivity extends Activity {
 	private TextView splash_version_name;
 	private RelativeLayout rl_splash_root;
 	private String versionName;
+	private AnimationSet mAnimationSet;
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         
+        //初始化界面。
         initView();
-        
+        //显示数据到界面。
         initDate();
         
         initEvent();
+        
+        //设置动画
+        initAnimation();
     }
+
+	private void initAnimation() {
+		RotateAnimation ra = new RotateAnimation(0.0f, 360.0f, 
+												Animation.RELATIVE_TO_SELF, 0.5f, 
+												Animation.RELATIVE_TO_SELF, 0.5f);
+		ra.setDuration(2000);
+		ra.setFillAfter(true);
+		
+		ScaleAnimation sa = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f, 
+												Animation.RELATIVE_TO_SELF, 0.5f, 
+												Animation.RELATIVE_TO_SELF, 0.5f);
+		sa.setDuration(2000);
+		sa.setFillAfter(true);
+		
+		AlphaAnimation aa=new AlphaAnimation(0.0f, 1.0f);
+		aa.setDuration(2000);
+		aa.setFillAfter(true);
+		
+		
+		mAnimationSet = new AnimationSet(false);
+		mAnimationSet.addAnimation(sa);
+		mAnimationSet.addAnimation(ra);
+		mAnimationSet.addAnimation(aa);
+		rl_splash_root.startAnimation(mAnimationSet);
+	}
 
 	/**
 	 * 初始化splash界面
