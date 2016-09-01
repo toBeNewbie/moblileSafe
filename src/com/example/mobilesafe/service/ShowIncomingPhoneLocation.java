36ea03a6e2f91e01a3ee5bd1cf70287e2bc036d6
toBeNewbie <1320425613@qq.com>
2016-09-01 08:59:14 +0800

    外拨电话归属地显示查询

--- src/com/example/mobilesafe/service/ShowIncomingPhoneLocation.java
+++ src/com/example/mobilesafe/service/ShowIncomingPhoneLocation.java
@@ -4,17 +4,20 @@ import com.example.mobilesafe.dao.AddressPhoneLocationDao;
 import com.example.mobilesafe.difineView.MyToast;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.widget.Toast;
 
 public class ShowIncomingPhoneLocation extends Service {
 
 	private TelephonyManager telephonyManager;
 	private PhoneStateListener listener;
 	private MyToast myToast;
+	private OutCallReceiver outCallReceiver;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -22,6 +25,20 @@ public class ShowIncomingPhoneLocation extends Service {
 		return null;
 	}
 
+	private class OutCallReceiver extends BroadcastReceiver{
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			
+			//显示外拨手机号码
+			String phoneNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+			String phoneLocation = AddressPhoneLocationDao.getPhoneMessage(phoneNumber);
+			myToast.show(phoneLocation);
+			
+		}
+	
+	}
+	
 	// 服务创建开始，
 	@Override
 	public void onCreate() {
@@ -30,8 +47,17 @@ public class ShowIncomingPhoneLocation extends Service {
 
 		// 注册显示来电归属地显示服务。
 		registerShowPhoneLocation();
-
+		
+		//注册外拨电话归属地服务。
+		registeOutCallReceiver();
+		
 		super.onCreate();
+	}
+
+	public void registeOutCallReceiver() {
+		outCallReceiver = new OutCallReceiver();
+		IntentFilter intentFilter = new IntentFilter(Intent.ACTION_NEW_OUTGOING_CALL);
+		registerReceiver(outCallReceiver, intentFilter);
 	}
 
 	public void registerShowPhoneLocation() {
@@ -52,7 +78,7 @@ public class ShowIncomingPhoneLocation extends Service {
 					break;
 
 				case TelephonyManager.CALL_STATE_OFFHOOK:// 通话状态
-					myToast.hiden();
+//					myToast.hiden();
 					break;
 				default:
 					break;
@@ -78,6 +104,9 @@ public class ShowIncomingPhoneLocation extends Service {
 	public void onDestroy() {
 		// 取消注册电话来电监听。
 		telephonyManager.listen(listener, PhoneStateListener.LISTEN_NONE);
+		
+		//取消外拨电话监听。
+		unregisterReceiver(outCallReceiver);
 		super.onDestroy();
 	}
 
