485203b6f59b7c4f0a6cfb3252a31e44e2440220
toBeNewbie <1320425613@qq.com>
2016-09-06 09:40:19 +0800

    软件设置，第三方分享逻辑编码

--- src/com/example/mobilesafe/activity/SoftwareManager.java
+++ src/com/example/mobilesafe/activity/SoftwareManager.java
@@ -31,6 +31,8 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 import com.example.mobilesafe.R;
 import com.example.mobilesafe.bean.AppInforBean;
@@ -190,7 +192,11 @@ public class SoftwareManager extends Activity {
 	
 	
 	protected void settingApp() {
-		System.out.println("settingApp"+clickedInforBean.getPackageName());
+		 
+		Intent setting=new Intent("android.settings.APPLICATION_DETAILS_SETTINGS");
+		setting.setData(Uri.parse("package:com.example.mobilesafe"));
+		startActivity(setting);
+		
 	}
 
 	protected void unIstallApp() {
@@ -227,10 +233,8 @@ public class SoftwareManager extends Activity {
 			userAppIntent.setData(Uri.parse("package:"+clickedInforBean.getPackageName()));
 			startActivity(userAppIntent);
 			
-			//刷新界面。
 		
 		}
-		System.out.println("unstallApp "+clickedInforBean.getPackageName());		
 	}
 
 	protected void startApp() {
@@ -239,10 +243,42 @@ public class SoftwareManager extends Activity {
 	}
 
 	protected void shareApp() {
-		System.out.println("shareApp"+clickedInforBean.getPackageName());		
+
+		showShare();
 	}
 
 	
+	
+	private void showShare() {
+		 ShareSDK.initSDK(this);
+		 OnekeyShare oks = new OnekeyShare();
+		 //关闭sso授权
+		 oks.disableSSOWhenAuthorize(); 
+
+		// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+		 //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+		 // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+		 oks.setTitle(getString(R.string.share));
+		 // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+		 oks.setTitleUrl("http://sharesdk.cn");
+		 // text是分享文本，所有平台都需要这个字段
+		 oks.setText("魔法革");
+		 //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+		 oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+		 // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+		 //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+		 // url仅在微信（包括好友和朋友圈）中使用
+		 oks.setUrl("http://sharesdk.cn");
+		 // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+		 oks.setComment("评论文本");
+		 // site是分享此内容的网站名称，仅在QQ空间使用
+		 oks.setSite(getString(R.string.app_name));
+		 // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+		 oks.setSiteUrl("http://sharesdk.cn");
+
+		// 启动分享GUI
+		 oks.show(this);
+		 }
 	
 	
 	
